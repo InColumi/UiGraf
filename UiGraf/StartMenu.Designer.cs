ba1c3ebd6683ac342d2cad5e047be82805e5a79a
max Icolumi <max77648@gmail.com>
2020-12-21 23:14:40 +0300

    Add project files.

---
+++ UiGraf/StartMenu.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace UiGraf
+{
+    partial class StartMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.открытьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьФайлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1176, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // открытьФайлToolStripMenuItem
+            // 
+            this.открытьФайлToolStripMenuItem.Name = "открытьФайлToolStripMenuItem";
+            this.открытьФайлToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.открытьФайлToolStripMenuItem.Text = "Открыть файл";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(416, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "123";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(758, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 40);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "123";
+            // 
+            // StartMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1176, 560);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "StartMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UI";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.StartMenu_MouseClick);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem открытьФайлToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ UiGraf/StartMenu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UiGraf
+{
+    public partial class StartMenu : Form
+    {
+        private List<Label> _labels;
+        private bool _isMouseDown;
+        public StartMenu()
+        {
+            _labels = new List<Label>();
+            InitializeComponent();
+
+            //Graphics gr = e.Graphics;
+            //Pen p = new Pen(Color.Blue, 5);// цвет линии и ширина
+            //Point p1 = new Point(5, 10);// первая точка
+            //Point p2 = new Point(40, 100);// вторая точка
+            //gr.DrawLine(p, p1, p2);// рисуем линию
+            //gr.Dispose();// освобождаем все ресурсы, связанные с отрисовкой
+
+        }
+
+        private void StartMenu_MouseClick(object sender, MouseEventArgs e)
+        {
+            AddLable(e);
+        }
+
+        private void AddLable(MouseEventArgs e)
+        {
+            Label label = new Label();
+            label.Text = (_labels.Count + 1).ToString();
+            label.BackColor = Color.LightGreen;
+            label.Location = new Point(e.X, e.Y);
+            label.Size = new Size(33, 40);
+            label.AutoSize = true;
+            label.Font = new Font("Segoe UI", 22, FontStyle.Bold);
+            Controls.Add(label);
+            _labels.Add(label);
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            _isMouseDown = true;
+        }
+
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Control c = sender as Control;
+            if (_isMouseDown)
+            {
+                c.Location = this.PointToClient(Control.MousePosition);
+            }
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _isMouseDown = false;
+        }
+
+    }
+}
